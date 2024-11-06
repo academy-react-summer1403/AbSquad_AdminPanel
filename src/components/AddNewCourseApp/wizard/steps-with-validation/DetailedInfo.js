ffd4029c2900be9dffb7231e9bf50f368b513a2c
Aiden <amir138204@yahoo.com>
2024-11-06 22:01:39 +0330

    Made The Submit Button Green

--- src/components/AddNewCourseApp/wizard/steps-with-validation/DetailedInfo.js
+++ src/components/AddNewCourseApp/wizard/steps-with-validation/DetailedInfo.js
@@ -191,16 +191,10 @@ const DetailedInfo = ({ stepper }) => {
               size={14}
               className="align-middle me-sm-25 me-0"
             ></ArrowLeft>
-            <span className="align-middle d-sm-inline-block d-none">
-              Previous
-            </span>
+            <span className="align-middle d-sm-inline-block d-none">قبلی</span>
           </Button>
-          <Button type="submit" color="primary" className="btn-next">
-            <span className="align-middle d-sm-inline-block d-none">Next</span>
-            <ArrowRight
-              size={14}
-              className="align-middle ms-sm-25 ms-0"
-            ></ArrowRight>
+          <Button type="submit" color="success" className="btn-submit">
+            <span className="align-middle d-sm-inline-block d-none">ثبت</span>
           </Button>
         </div>
       </Form>
