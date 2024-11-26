1d117cbb251089ef0ceaf61da8c6af3ac5f1bf02
Aiden <amir138204@yahoo.com>
2024-11-27 02:14:14 +0330

    minor edits

--- src/components/CourseApp/CourseDetails/ReserveList.js
+++ src/components/CourseApp/CourseDetails/ReserveList.js
@@ -1,5 +1,5 @@
 // ** Reactstrap Imports
-import { Badge, Card, CardHeader, Progress, Button } from "reactstrap";
+import { Badge, Card, Button } from "reactstrap";
 
 // ** Third Party Components
 import { ChevronDown } from "react-feather";
@@ -7,13 +7,6 @@ import DataTable from "react-data-table-component";
 
 // ** Custom Components
 import Avatar from "@components/avatar";
-
-// ** Label Images
-import xdLabel from "@src/assets/images/icons/brands/xd-label.png";
-import vueLabel from "@src/assets/images/icons/brands/vue-label.png";
-import htmlLabel from "@src/assets/images/icons/brands/html-label.png";
-import reactLabel from "@src/assets/images/icons/brands/react-label.png";
-import sketchLabel from "@src/assets/images/icons/brands/sketch-label.png";
 
 // ** Styles
 import "@styles/react/libs/tables/react-dataTable-component.scss";
@@ -32,73 +25,6 @@ const statusColor = {
   true: "success",
   false: "danger",
 };
-
-const projectsArr = [
-  {
-    progress: 60,
-    hours: "210:30h",
-    progressColor: "info",
-    totalTasks: "233/240",
-    subtitle: "React Project",
-    title: "BGC eCommerce App",
-    img: reactLabel,
-  },
-  {
-    hours: "89h",
-    progress: 15,
-    totalTasks: "9/50",
-    progressColor: "danger",
-    subtitle: "UI/UX Project",
-    title: "Falcon Logo Design",
-    img: xdLabel,
-  },
-  {
-    progress: 90,
-    hours: "129:45h",
-    totalTasks: "100/190",
-    progressColor: "success",
-    subtitle: "Vuejs Project",
-    title: "Dashboard Design",
-    img: vueLabel,
-  },
-  {
-    hours: "45h",
-    progress: 49,
-    totalTasks: "12/86",
-    progressColor: "warning",
-    subtitle: "iPhone Project",
-    title: "Foodista mobile app",
-    img: sketchLabel,
-  },
-
-  {
-    progress: 73,
-    hours: "67:10h",
-    totalTasks: "234/378",
-    progressColor: "info",
-    subtitle: "React Project",
-    title: "Dojo React Project",
-    img: reactLabel,
-  },
-  {
-    progress: 81,
-    hours: "108:39h",
-    totalTasks: "264/537",
-    title: "HTML Project",
-    progressColor: "success",
-    subtitle: "Crypto Website",
-    img: htmlLabel,
-  },
-  {
-    progress: 78,
-    hours: "88:19h",
-    totalTasks: "214/627",
-    progressColor: "success",
-    subtitle: "Vuejs Project",
-    title: "Vue Admin template",
-    img: vueLabel,
-  },
-];
 
 export const columns = [
   {
@@ -186,7 +112,6 @@ export const columns = [
 
                 // Getting StudentId
                 row.setStudentId(row.studentId);
-                console.log(row);
               }}
               color="success"
             >
@@ -240,7 +165,7 @@ const ReserveList = ({ courseDetail }) => {
   // Catchong Course Group Detail
   useEffect(() => {
     if (courseGroupId) {
-      // handleAcceptReserve(courseGroupId, courseDetail.courseId, studentId);
+      handleAcceptReserve(courseGroupId, courseDetail.courseId, studentId);
     }
   }, [courseGroupId]);
 
@@ -311,15 +236,6 @@ const ReserveList = ({ courseDetail }) => {
               setGroupId: setGroupId,
             };
           })}
-          // subHeaderComponent={
-          //   <CustomHeader
-          //     store={store}
-          //     searchTerm={searchTerm}
-          //     setSearchParams={setSearchParams}
-          //     handleFilter={() => {}}
-          //     handlePerRow={() => {}}
-          //   />
-          // }
         />
       </div>
     </Card>
