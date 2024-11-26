d3340bff35d3540baa465f3d7dd7a92681114e99
Aiden <amir138204@yahoo.com>
2024-11-27 00:25:21 +0330

    made things improve

--- src/@core/services/API/AllCoursesAdmin/allCourse.api.js
+++ src/@core/services/API/AllCoursesAdmin/allCourse.api.js
@@ -1,7 +1,8 @@
 ﻿import http from "../../../services/Interceptor";
-export const AllCourseAdmin = async (searchParams = "") => {
+export const AllCourseAdmin = async (searchParams = {}) => {
   try {
-    const res = await http.get(`/Course/CourseList` + searchParams);
+    const res = await http.get(`/Course/CourseList`, { params: searchParams });
+    console.log(res);
     return res;
   } catch (error) {}
 };
