94002c1f96dc2634451d4242dda1e5ddf1a9becc
Aiden <amir138204@yahoo.com>
2024-12-03 16:34:56 +0330

    Made AddTech

---
+++ src/components/TechnologyApp/AddTech.js
@@ -0,0 +1,138 @@
+﻿// ** React Imports
+import { Fragment, useEffect, useState } from "react";
+// ** Utils
+import { selectThemeColors } from "@utils";
+// ** Reactstrap Imports
+import {
+  Row,
+  Col,
+  Modal,
+  Label,
+  Button,
+  ModalBody,
+  InputGroup,
+  Input,
+  ModalHeader,
+} from "reactstrap";
+
+import classnames from "classnames";
+import { useForm, Controller } from "react-hook-form";
+import AddTechApi from "../../@core/services/API/Technology/add.tech.api";
+
+const AddTech = ({ setShow, show }) => {
+  //   States
+  const [parameters, setParameters] = useState({
+    parentId: null,
+    iconAddress: "testi",
+  });
+
+  const handleAddTech = async (data) => {
+    await AddTechApi(data);
+  };
+  useEffect(() => {
+    if (parameters.describe) handleAddTech(parameters);
+  }, [parameters]);
+
+  // ** Hooks
+  const {
+    reset,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({});
+
+  const onSubmit = (data) => {
+    setParameters({ ...data, ...parameters });
+    setShow(!show);
+  };
+
+  return (
+    <Fragment>
+      <Modal
+        isOpen={show}
+        toggle={() => setShow(!show)}
+        className="modal-dialog-centered"
+      >
+        <ModalHeader
+          className="bg-transparent"
+          toggle={() => setShow(!show)}
+        ></ModalHeader>
+        <ModalBody className="px-sm-5 mx-50 pb-5">
+          <h1 className="text-center mb-1">ساخت تکنولوژی</h1>
+
+          <Row
+            tag="form"
+            className="gy-1 gx-2 mt-75"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <Col xs={12}>
+              <Label className="form-label" for="techName">
+                نام تکنولوژی
+              </Label>
+
+              <InputGroup>
+                <Controller
+                  name="techName"
+                  control={control}
+                  render={({ field }) => {
+                    return (
+                      <Input
+                        {...field}
+                        id="techName"
+                        value={field.value}
+                        onChange={field.onChange}
+                        placeholder="نام تکنولوژی را وارد کنید..."
+                        className={classnames("form-control")}
+                      />
+                    );
+                  }}
+                />
+              </InputGroup>
+            </Col>
+            <Col xs={12}>
+              <Label className="form-label" for="describe">
+                درباره تکنولوژی
+              </Label>
+
+              <InputGroup>
+                <Controller
+                  name="describe"
+                  control={control}
+                  render={({ field }) => {
+                    return (
+                      <Input
+                        {...field}
+                        id="describe"
+                        value={field.value}
+                        onChange={field.onChange}
+                        placeholder="توضیحات تکنولوژی را وارد کنید..."
+                        className={classnames("form-control")}
+                      />
+                    );
+                  }}
+                />
+              </InputGroup>
+            </Col>
+            <Col>
+              <Button type="submit" className="me-1" color="primary">
+                ثبت
+              </Button>
+              <Button
+                color="secondary"
+                outline
+                onClick={() => {
+                  setShow(!show);
+                  reset();
+                }}
+              >
+                خروج
+              </Button>
+            </Col>
+          </Row>
+        </ModalBody>
+      </Modal>
+    </Fragment>
+  );
+};
+
+export { AddTech };
---
+++ src/components/TechnologyApp/list/Table.js
@@ -0,0 +1,199 @@
+// ** React Imports
+import { Fragment, useState, useEffect } from "react";
+
+// ** Table Columns
+import { columns } from "./columns";
+
+// ** Third Party Components
+
+import ReactPaginate from "react-paginate";
+import DataTable from "react-data-table-component";
+import { ChevronDown } from "react-feather";
+
+// ** Reactstrap Imports
+import { Row, Col, Card, Input, Button } from "reactstrap";
+
+// ** Styles
+import "@styles/react/libs/react-select/_react-select.scss";
+import "@styles/react/libs/tables/react-dataTable-component.scss";
+import GetTechApi from "../../../@core/services/API/Technology/get.tech.api";
+import { AddTech } from "../AddTech";
+
+// ** Get Semester Api *************************************************************************************
+
+// ** Table Header
+const CustomHeader = ({
+  searchTerm,
+  setSearchTerm,
+  setParameters,
+  parameters,
+  rowsPerPage,
+  setRowsPerPage,
+  showAdd,
+  setShowAdd,
+}) => {
+  // Search
+  const handleSearch = (val) => {};
+  const [show, setShow] = useState(false);
+  return (
+    <div className="invoice-list-table-header w-100 me-1 ms-50 mt-2 mb-75">
+      <Row>
+        <Col xl="6" className="d-flex align-items-center p-0">
+          <div className="d-flex align-items-center w-100">
+            <label htmlFor="rows-per-page">تعداد:</label>
+            <Input
+              className="mx-50"
+              onChange={(e) => {
+                setRowsPerPage(parseInt(e.target.value));
+              }}
+              type="select"
+              id="rows-per-page"
+              style={{ width: "5rem" }}
+            >
+              <option value="10">10</option>
+              <option value="25">25</option>
+              <option value="50">50</option>
+            </Input>
+          </div>
+        </Col>
+        <Col
+          xl="6"
+          className="d-flex align-items-sm-center justify-content-xl-end justify-content-start flex-xl-nowrap flex-wrap flex-sm-row flex-column pe-xl-1 p-0 mt-xl-0 mt-1"
+        >
+          <div className="d-flex align-items-center mb-sm-0 mb-1 me-1">
+            <label className="mb-0" htmlFor="search-invoice">
+              جستجو:
+            </label>
+            <Input
+              id="search-invoice"
+              className="ms-50 w-100"
+              type="text"
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+                handleSearch(searchTerm);
+              }}
+            />
+          </div>
+
+          <div className="d-flex align-items-center table-header-actions">
+            <Button
+              className="add-new-user"
+              color="primary"
+              onClick={() => {
+                setShow(true);
+              }}
+            >
+              ساخت تکنولوژی
+            </Button>
+          </div>
+        </Col>
+        <AddTech show={show} setShow={setShow} />
+      </Row>
+    </div>
+  );
+};
+
+const TechList = () => {
+  // ** States
+  const [searchTerm, setSearchTerm] = useState("");
+  const [parameters, setParameters] = useState({});
+  const [currentPage, setCurrentPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [showAdd, setShowAdd] = useState(false);
+  useEffect(() => {
+    setParameters({ ...parameters, PageNumber: currentPage + 1 });
+  }, [currentPage]);
+
+  // ** Custom Pagination
+  const CustomPagination = () => {
+    const count = Number(Math.ceil(10 / rowsPerPage));
+    const handlePagination = (pageNum) => {
+      setCurrentPage(pageNum);
+    };
+    return (
+      <ReactPaginate
+        previousLabel={""}
+        nextLabel={""}
+        pageCount={count || 1}
+        activeClassName="active"
+        forcePage={currentPage != 0 ? currentPage : 0}
+        onPageChange={(page) => handlePagination(page.selected)}
+        pageClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        nextClassName={"page-item next"}
+        previousClassName={"page-item prev"}
+        previousLinkClassName={"page-link"}
+        pageLinkClassName={"page-link"}
+        containerClassName={
+          "pagination react-paginate justify-content-end my-2 pe-1"
+        }
+      />
+    );
+  };
+
+  // ** Table data to render
+
+  const [refresh, setRefresh] = useState(false);
+  const [techs, setTechs] = useState([]);
+  const handleGetTechs = async () => {
+    const res = await GetTechApi();
+    setTechs(res);
+  };
+  useEffect(() => {
+    handleGetTechs();
+  }, []);
+
+  // searchTerm ** copy this above after making a button for search
+  return (
+    <Fragment>
+      <Card className="overflow-hidden">
+        <div className="react-dataTable">
+          <DataTable
+            noHeader
+            subHeader
+            sortServer
+            pagination
+            responsive
+            selectableRows
+            paginationServer
+            columns={columns}
+            onSort={() => {}}
+            sortIcon={<ChevronDown />}
+            className="react-dataTable"
+            paginationComponent={() => (
+              <CustomPagination
+                currentPage={currentPage}
+                setParameters={setParameters}
+                parameters={parameters}
+              />
+            )}
+            data={
+              techs
+                ? techs.map((it) => {
+                    return { ...it };
+                  })
+                : ""
+            }
+            subHeaderComponent={
+              <CustomHeader
+                setSearchTerm={setSearchTerm}
+                searchTerm={searchTerm}
+                setParameters={setParameters}
+                parameters={parameters}
+                setRowsPerPage={setRowsPerPage}
+                setShowAdd={setShowAdd}
+                showAdd={showAdd}
+                rowsPerPage={rowsPerPage}
+                handleFilter={() => {}}
+                handlePerRow={() => {}}
+              />
+            }
+          />
+        </div>
+      </Card>
+    </Fragment>
+  );
+};
+
+export default TechList;
