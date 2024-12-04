f93246f20287183efd23852d8167e425b5012c03
Aiden <amir138204@yahoo.com>
2024-12-05 00:10:58 +0330

    Dashboard done

---
+++ src/@core/services/API/Dashboard/get.techReport.api.js
@@ -0,0 +1,8 @@
+﻿import http from "../../../services/Interceptor";
+export const GetDashboardTechReport = async () => {
+  try {
+    const res = await http.get(`/Report/DashboardTechnologyReport
+`);
+    return res;
+  } catch (error) {}
+};
